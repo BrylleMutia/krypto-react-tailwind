26a8f683cd9345c6d6bd8fa3275b2ff83c150fb2
bryllemutia <bryllemutia3@gmail.com>
2023-05-03 11:01:55 +0800

    added hover style for button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ styles, text, adaptive = true, ...props }) => {
    return (
       <button
-         className={`font-outfit font-semibold rounded-full tracking-wider uppercase px-6 py-2 ${
+         className={`font-outfit font-semibold rounded-full tracking-wider uppercase px-6 py-2 hover:bg-accent transition-all ${
             adaptive && "md:px-12 md:py-3.5 text-button"
          } ${styles}`}
          {...props}
